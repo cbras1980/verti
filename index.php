7ae0a0c362ec76487d34939efd3aed4a24643fd9
Carlos Bras <cbras1980@gmail.com>
2021-11-22 16:15:43 +0100

    f

--- index.php
+++ index.php
@@ -14,6 +14,24 @@
 	<body class="is-preload no-sidebar">
 		<div id="page-wrapper">
 
+      <!-- Features -->
+			<div id="banner-wrapper">
+				<div id="banner" class="box container">
+					<div class="row">
+						<div class="col-7 col-12-medium">
+							<h2>Hi. This is Verti.</h2>
+							<p class="large icon solid fa-temperature-half">It's a free responsive site template by HTML5 UP</p>
+						</div>
+						<div class="col-5 col-12-medium">
+							<ul>
+								<li><a href="#" class="button large icon solid fa-temperature-half"></a></li>
+								<li><a href="#" class="button alt large icon solid fa-question-circle">More info</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+
 			<!-- Features -->
 				<div id="features-wrapper">
 					<div class="container">
